0fc3821f87594274985d778920d634651c439253
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-23 09:38:49 +0900

    updated

--- app/views.py
+++ app/views.py
@@ -4,7 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("<h3>Hello World</h3>")
 
 def job_detail(request, id):
-    return HttpResponse(f"Job detail page{id}")
+    #return HttpResponse(f"<h1>Job detail page{id}</h1>")
+    site = "https://google.com"
+    return HttpResponse(f"Visit <a href={site}>Google here</a>")
