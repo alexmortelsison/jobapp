3eeaa8df0af89ffe749656e090a84836312ce176
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-23 10:34:01 +0900

    added variables

--- app/views.py
+++ app/views.py
@@ -2,6 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+job_title = [
+    "First job",
+    "Second job"
+]
+
+job_description = [
+    "First job description",
+    "Second job description"
+]
 
 def hello(request):
     return HttpResponse("<h3>Hello World</h3>")
